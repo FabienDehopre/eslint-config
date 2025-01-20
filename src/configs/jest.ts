6f7eb2df70aa7f40ee7a6bf1af9332225c8a2db0
Fabien Dehopré <fabien@dehopre.com>
2025-01-20 14:55:41 +0100

    feat: add jest config

---
+++ src/configs/jest.ts
@@ -0,0 +1,42 @@
+import jest from 'eslint-plugin-jest';
+import jestDom from 'eslint-plugin-jest-dom';
+import testingLibrary from 'eslint-plugin-testing-library';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+import { GLOB_TESTS } from '../globs';
+
+export default tseslint.config({
+  name: 'fabdeh/jest/rules',
+  plugins: {
+    jest,
+  },
+  languageOptions: {
+    globals: {
+      ...globals.node,
+      ...globals.jest,
+    },
+  },
+  files: [GLOB_TESTS],
+  extends: [
+    jest.configs['flat/recommended'],
+    jest.configs['flat/style'],
+    jestDom.configs['flat/recommended'],
+    testingLibrary.configs['flat/angular'],
+  ],
+  rules: {
+    'max-classes-per-file': 'off',
+    'max-lines': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/unbound-method': 'off',
+    'jest/consistent-test-it': ['error', { fn: 'test' }],
+    'jest/prefer-spy-on': 'error',
+    'jest/require-top-level-describe': 'error',
+    'jest/unbound-method': 'error',
+    'unicorn/no-null': 'off',
+  },
+});
