2ad680895719b17e4e62a1fbe489585e87a17d46
Fabien Dehopré <fabien@dehopre.com>
2025-01-22 14:00:43 +0100

    refactor: use process.cwd() instead of import.meta.dirname

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,7 +9,7 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: process.cwd(),
       },
     },
   },
