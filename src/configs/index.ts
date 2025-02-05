54c26cd770ba471285f3f73ba046af10746f6e8a
Fabien Dehopré <fabien@dehopre.com>
2025-02-05 14:42:53 +0100

    refactor: remove old base config

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,20 +1,9 @@
-// old exports to migrate and remove later
-export { default as angular } from './angular';
-export { default as base } from './base';
-export { default as jest } from './jest';
-export * as ngrx from './ngrx';
-export { default as tailwindcss } from './tailwindcss';
-export { default as vitest } from './vitest';
-
-/* eslint-disable perfectionist/sort-exports -- to be removed later */
-// new exports
 export * from './comments';
 export * from './ignore';
 export * from './imports';
-export * from './jsdoc';
 export * from './javascript';
+export * from './jsdoc';
 export * from './perfectionist';
 export * from './stylistic';
 export * from './typescript';
 export * from './unicorn';
-/* eslint-enable perfectionist/sort-exports */
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-// TODO remove this one
-export * as configs from './configs';
-
 export * from './configs';
 export { fabdehConfig } from './factory';
 export { GLOB_HTML, GLOB_JS, GLOB_SRC, GLOB_TESTS, GLOB_TS } from './globs';
