1e7c453707065ab7ed0c6781e4de131941cc9102
Fabien Dehopré <fabien@dehopre.com>
2025-02-06 14:54:34 +0100

    chore: move peer dependencies to dependencies as they are all required

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,22 +1,3 @@
-declare module '@nx/eslint-plugin' {
-  import type { TSESLint } from '@typescript-eslint/utils';
-
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  declare const _default: {
-    configs: {
-      'flat/base': TSESLint.FlatConfig.ConfigArray;
-      'flat/typescript': TSESLint.FlatConfig.ConfigArray;
-      'flat/javascript': TSESLint.FlatConfig.ConfigArray;
-      'flat/angular': TSESLint.FlatConfig.ConfigArray;
-      'flat/angular-template': TSESLint.FlatConfig.ConfigArray;
-    };
-    rules: Record<string, unknown>; // just need to be declared... we don't care about the rules
-  };
-
-  // eslint-disable-next-line no-restricted-syntax
-  export = _default;
-}
-
 declare module '@eslint-community/eslint-plugin-eslint-comments' {
   import type { ESLint, Linter } from 'eslint';
 
