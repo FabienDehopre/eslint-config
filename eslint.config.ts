d6e9b6fa4dd595b44deb40bebadf6eaefa59c8da
Fabien Dehopré <fabien@dehopre.com>
2025-02-06 10:53:43 +0100

    chore: no need to explicitly disable rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,13 +2,4 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 import { createConfig } from './src';
 
-export default createConfig(
-  {
-    angular: false,
-    ngrx: false,
-    jest: false,
-    vitest: false,
-    tailwindcss: false,
-  },
-  { name: 'eslint-config-prettier', ...eslintConfigPrettier }
-);
+export default createConfig({}, { name: 'eslint-config-prettier', ...eslintConfigPrettier });
