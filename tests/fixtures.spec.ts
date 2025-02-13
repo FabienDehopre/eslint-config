f352ebd28acbac4a4825f066d18a7622e15e6e1a
Fabien Dehopré <fabien@dehopre.com>
2025-02-13 16:16:51 +0100

    test: rewrite the way test are executed

--- tests/fixtures.spec.ts
+++ tests/fixtures.spec.ts
@@ -1,45 +1,44 @@
 import type { ConfigWithExtends } from 'typescript-eslint';
 import type { CreateConfigOptions } from '../src';
 
+import { existsSync } from 'node:fs';
+import { readFile, rm, writeFile } from 'node:fs/promises';
 import { join, resolve } from 'node:path';
 
 import { execa } from 'execa';
 import fg from 'fast-glob';
-import fs from 'fs-extra';
+import { copy, remove } from 'fs-extra/esm';
 import { afterAll, beforeAll, describe, test } from 'vitest';
+
+interface TestParams {
+  name: string;
+  config: CreateConfigOptions;
+  items?: ConfigWithExtends[];
+}
 
 describe('fixtures', () => {
   beforeAll(async () => {
-    await fs.rm('_fixtures', { recursive: true, force: true });
+    await rm('_fixtures', { recursive: true, force: true });
   });
 
   afterAll(async () => {
-    await fs.rm('_fixtures', { recursive: true, force: true });
+    await rm('_fixtures', { recursive: true, force: true });
   });
 
-  runWithConfig('all', { /* TODO */ });
-});
-
-/**
- * Execute the test {@link name} with the specified {@link config} and optionally the specified additional {@link items}.
- *
- * @param name - The name of the test.
- * @param config - The configuration to use for the test.
- * @param items - The optional additional ESLint configuration.
- */
-function runWithConfig(name: string, config: CreateConfigOptions, ...items: ConfigWithExtends[]): void {
-  // eslint-disable-next-line vitest/require-top-level-describe,vitest/valid-title
-  test.concurrent(name, async ({ expect }) => {
+  test.concurrent.for([
+    { name: 'all', config: {} },
+    { name: 'js', config: {}, items: [] },
+  ] as TestParams[])('$name', async ({ name, config, items }, { expect }) => {
     const from = resolve('fixtures/input');
     const output = resolve('fixtures/output', name);
     const target = resolve('_fixtures', name);
 
-    await fs.copy(from, target, {
+    await copy(from, target, {
       filter: (src) => {
         return !src.includes('node_modules');
       },
     });
-    await fs.writeFile(join(target, 'eslint.config.js'), `
+    await writeFile(join(target, 'eslint.config.js'), `
 // @eslint-disable
 import { createConfig } from '@fabdeh/eslint-config';
 
@@ -60,12 +59,12 @@ export default createConfig(
     });
 
     await Promise.all(files.map(async (file) => {
-      const content = await fs.readFile(join(target, file), 'utf8');
-      const source = await fs.readFile(join(from, file), 'utf8');
+      const content = await readFile(join(target, file), 'utf8');
+      const source = await readFile(join(from, file), 'utf8');
       const outputPath = join(output, file);
       if (content === source) {
-        if (fs.existsSync(outputPath)) {
-          await fs.remove(outputPath);
+        if (existsSync(outputPath)) {
+          await remove(outputPath);
         }
 
         return;
@@ -73,5 +72,5 @@ export default createConfig(
 
       await expect.soft(content).toMatchFileSnapshot(join(output, file));
     }));
-  }, 30_000);
-}
+  });
+});
