97469550975a7b37d3ecd318799da12986f91b66
Fabien Dehopré <fabien@dehopre.com>
2025-08-07 17:58:48 +0200

    style: format the eslint config file

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from './src';
 
 export default defineConfig(
-  { formatters: true, typescript: { enableErasableSyntaxOnly: true, useRelaxedNamingConventionForCamelAndPascalCases: true }, type: 'lib' },
+  {
+    formatters: true,
+    typescript: {
+      enableErasableSyntaxOnly: true,
+      useRelaxedNamingConventionForCamelAndPascalCases: true,
+    },
+    type: 'lib',
+  },
   {
     ignores: ['fixtures', '_fixtures'],
   }
