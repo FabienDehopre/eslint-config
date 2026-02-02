964e0f1c3fe4a452dfd6cac2bed07e2c81cf1226
Fabien Dehopré <fabien@dehopre.com>
2026-02-02 15:50:49 +0100

    refactor: simplify indent setting on both toml and yaml configs

--- src/configs/yaml.ts
+++ src/configs/yaml.ts
@@ -61,7 +61,7 @@ export async function yaml(options: FilesOptions & OverridesOptions & StylisticO
               'yaml/flow-mapping-curly-spacing': 'error',
               'yaml/flow-sequence-bracket-newline': 'error',
               'yaml/flow-sequence-bracket-spacing': 'error',
-              'yaml/indent': ['error', indent === 'tab' ? 2 : indent],
+              'yaml/indent': ['error', indent],
               'yaml/key-spacing': 'error',
               'yaml/no-tab-indent': 'error',
               'yaml/quotes': ['error', { avoidEscape: true, prefer: quotes === 'backtick' ? 'single' : quotes }],
