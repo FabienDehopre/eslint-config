a07853b5575f9f6a1f971c56b1b272b60872654a
Fabien Dehopré <fabien@dehopre.com>
2026-04-14 15:44:05 +0200

    test: remove default timeout
    a custom timeout is computed based on the platform in the fixtures tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    testTimeout: 60_000,
-  },
   server: {
     watch: {
       ignored: ['**/fixtures/**', '**/_fixtures/**'],
