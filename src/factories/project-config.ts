50af5410fc6f859d7e80b635e361ed021b4cda1f
Fabien Dehopré <fabien@dehopre.com>
2026-04-15 15:32:52 +0200

    fix: make sure  extends the workspace config

--- src/factories/project-config.ts
+++ src/factories/project-config.ts
@@ -118,6 +118,7 @@ export function defineProjectConfig(
   let composer = new FlatConfigComposer<TypedConfig, ConfigNames>();
   composer = composer
     .append(
+      baseConfig,
       ...configs,
       ...userConfigs
     );
